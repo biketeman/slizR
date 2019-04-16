7c583392af2ca7919f8411887d0c0857d85dd334
Alexis Lagniel <a_lagniel@etu-webschoolfactory.fr>
2019-04-16 19:26:09 +0200

    adding tokens

--- components/checkLogin.js
+++ components/checkLogin.js
@@ -3,27 +3,29 @@ const config = require(`../config/${env}`)
 const db = config.DB;
 const bcrypt = require('bcrypt');
 
+
 module.exports = function(req, res) {
     let username = req.body.username
     let password = req.body.password
 
     db('users').select ('username', 'password')
     .where({
-        username: username
-    }) 
+        'username': username
+    })
     .then((row) => {
-        console.log('')
         let auth = bcrypt.compareSync(password, row[0].password)
-
+        if(auth != true){
+            return res.json({
+                'status_code': 401,
+                'message': 'Acess denied',
+                'auth': auth
+            }) 
+        }
         return res.json({
-            'status_code': 200,
+            'status_code': 201,
             'message': 'Successfully logged in',
-            'username': username,
-            'input password': password,
-            'bdd password': row[0].password,
             'auth': auth
         })
-
     })
     .catch(function(err){
 
--- components/postUser.js
+++ components/postUser.js
@@ -2,22 +2,33 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(`../config/${env}`)
 const db = config.DB;
 const bcrypt = require('bcrypt');
+const hat = require('hat');
+
 
 module.exports = function(req, res) {
-
+		username = req.body.username
 		password = req.body.password
-    let salts = bcrypt.genSaltSync(10);
+		email = req.body.email,
+		apikey = hat()
+		apitoken = hat()
+    	let salts = bcrypt.genSaltSync(10);
 		let hashed_password = bcrypt.hashSync(password, salts);
 		
 		db('users').insert ({
-			username: req.body.username,
+			username: username,
 			password: hashed_password,
-			email: req.body.email,
+			email: email,
+			api_key: apikey,
+			api_token: apitoken
 		}) 
 		.then((row) => {
 			return res.json({
 				'status_code': 201,
-				'message': 'user successfully added'
+				'message': 'user successfully added',
+				'username': username,
+				'email' : email,
+				'api_key': apikey,
+				'api_token': apitoken,
 			})
 		})
 		.catch(function(err){
